4baea62afe7fe2494be7996be2d5a5de37099788
Aniket Jain <jaina290@gmail.com>
2024-07-29 01:04:18 +0530

    third  git push

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,41 @@
-import restaurants from "../Utils/mockdata";
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurant] = useState(restaurants);
+  const [listOfRestaurants, setListOfRestaurant] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfRestaurant(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
   return (
     <div className="body">
       <div className="filter">
+        <input className="search-box"></input>
+        <button className="search-btn">Submit</button>
+
         <button
           className="filter-btn"
           onClick={() => {
-            const filteredlist = restaurants.filter(
+            const filteredlist = listOfRestaurants.filter(
               (res) => res.info.avgRating > 4.2
             );
             setListOfRestaurant(filteredlist);
           }}
         >
           Top Rated Restaurant
+        </button>
+        <button className="reset" onClick={fetchData}>
+          Reset 🔄
         </button>
       </div>
       <div className="res-container">
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import { LOGO_URL } from "../Utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
   return (
     <div className="header">
       <img className="logo" src={LOGO_URL} alt="logo" />
@@ -10,6 +12,17 @@ const Header = () => {
         <li>Contact Us</li>
         <li>Menu</li>
         <li>Cart</li>
+        <li>
+          <button
+            className="login-btn"
+            style={btnName === "Login" ? { color: "green" } : { color: "red" }}
+            onClick={() => {
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
+        </li>
       </ul>
     </div>
   );
