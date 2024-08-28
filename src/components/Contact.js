a4365be2d45a3c457604503e0ba148e1213a4ea5
Aniket Jain <jaina290@gmail.com>
2024-08-28 23:06:39 +0530

    unit testing the compoent

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,23 @@
 const Contact = () => {
   return (
     <div>
-      <h1>Hello from Contact Us page</h1>
-      <h2>
+      <h1 className="font-bold text-3xl p-2 m-2">Contact Us page</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="name"
+          className="border border-black p-2 m-2"
+        ></input>
+        <input
+          type="text"
+          placeholder="message"
+          className="border border-black p-2 m-2"
+        ></input>
+        <button className="border border-black p-2 m-2 bg-gray-300 rounded-lg">
+          Submit
+        </button>
+      </form>
+      <h2 className="m-2 text-sm">
         Please reach out to fooddelivery420@hotmail.com for your queries &
         concerns 😁
       </h2>
---
+++ src/components/__tests__/Contact.test.js
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import Contact from "../Contact";
+import "@testing-library/jest-dom";
+
+describe("Contact us page test cases", () => {
+  test("should load contact us component", () => {
+    render(<Contact />);
+
+    const heading = screen.getAllByRole("heading");
+    console.log(heading);
+
+    expect(heading.length).toBe(2);
+  });
+
+  test("should load button inside contact us page component", () => {
+    render(<Contact />);
+
+    const button = screen.getByRole("button");
+
+    expect(button).toBeInTheDocument();
+  });
+
+  test("should load input placeholder name in contact us page component", () => {
+    render(<Contact />);
+    //Querying
+    const inputName = screen.getByPlaceholderText("name");
+    //Assertion
+    expect(inputName).toBeInTheDocument();
+  });
+});
